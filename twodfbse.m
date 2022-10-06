e5b6076ff112c0a0281fde8c3a3a263648c03f68
PRADEEP KUMAR CHAUDHARY <98513318+pkciiti@users.noreply.github.com>
2022-10-06 10:35:17 +0530

    Update twodfbse.m

--- twodfbse.m
+++ twodfbse.m
@@ -7,19 +7,19 @@ alfa=besselzero(0,M);
 K=2/(M*M);
 
 Bes=besselj(0,([1:M]'.*repmat(alfa/M,M,1)))./(besselj(1,repmat(alfa,M,1))).^2;
-if dim==1 %% Apply FBSE row-wise
+if dim==1 
 Aimg=A.*img;
 Cofcol=K*Aimg*Bes;
 
 % col
-elseif dim==2  %% Apply FBSE column-wise
+elseif dim==2  
 imgcol=img';
 Aimgcol=A.*imgcol;
 Cofcol=K*Aimgcol*Bes;
 Cofcol=Cofcol';
 
 % 2d
-elseif dim==3  %% Apply FBSE row-wise followed by column-wise
+elseif dim==3  
 Aimg=A.*img;
 Cof=K*Aimg*Bes;
 imgcol=Cof';
